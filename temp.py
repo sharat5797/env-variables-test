b69017b3852ca15d5b1e72e94109789e86d57048
Sharat Hegde <sharath@testvagrant.com>
2024-01-03 15:55:51 +0530

    modified temp.py

--- temp.py
+++ temp.py
@@ -1,15 +1,5 @@
-# get_email.py
+# temp.py
 import os
-from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Retrieve the email variable
-email = os.getenv("EMAIL")
-
-if email:
-    print(f"Email from .env: {email}")
-else:
-    print("Email not found in .env")
-#hellow comment
+email = os.getenv("EMAIL", "default_value_if_not_set")
+print(f"Email from env: {email}")
